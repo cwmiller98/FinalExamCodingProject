21fb0e38af696af11e24b7e137ea5cdf8b9c96b0
CharlieMiller <CharlieMiller@172.16.64.115>
2019-05-22 14:57:18 -0400

    Imports added

--- StudentLoanCalc/src/main/java/app/helper/Loan.java
+++ StudentLoanCalc/src/main/java/app/helper/Loan.java
@@ -1,5 +1,9 @@
 package app.helper;
+import app.StudentCalc;
+import app.controller.LoanCalcViewController;
+import java.util.ArrayList;
 
 public class Loan {
+	
 
 }
--- StudentLoanCalc/src/main/java/app/helper/Payment.java
+++ StudentLoanCalc/src/main/java/app/helper/Payment.java
@@ -1,5 +1,11 @@
 package app.helper;
+import app.StudentCalc;
+import app.controller.LoanCalcViewController;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+import org.apache.poi.ss.formula.functions.Finance;
 
 public class Payment {
+
 
 }
