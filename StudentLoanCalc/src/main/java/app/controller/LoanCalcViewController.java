ba36920427abc43ec8f9f43560a423c136d17d04
CharlieMiller <CharlieMiller@172.16.64.115>
2019-05-23 17:44:05 -0400

    Commit

--- StudentLoanCalc/src/main/java/app/controller/LoanCalcViewController.java
+++ StudentLoanCalc/src/main/java/app/controller/LoanCalcViewController.java
@@ -3,11 +3,18 @@ package app.controller;
 import app.StudentCalc;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.DatePicker;
 import app.helper.Loan;
@@ -24,7 +31,7 @@ public class LoanCalcViewController implements Initializable   {
 
 	
 	@FXML
-	private Label lblTotalPayemnts;
+	private Label lblTotalPayments;
 	
 	@FXML
 	private DatePicker PaymentStartDate;
@@ -40,6 +47,36 @@ public class LoanCalcViewController implements Initializable   {
 	
 	@FXML
 	private TextField NbrOfYears;
+	
+	@FXML
+	private DatePicker fstPmtDate;
+	
+	@FXML
+	private TextField addPmt;
+	
+	@FXML
+	private TableView tblV;
+	
+	@FXML
+	private TableColumn pmtNbrCol;
+	
+	@FXML
+	private TableColumn pmtDateCol;
+	
+	@FXML
+	private TableColumn pmtCol;
+	
+	@FXML
+	private TableColumn addPmtCol;
+	
+	@FXML
+	private TableColumn interestCol;
+	
+	@FXML
+	private TableColumn principleCol;
+	
+	@FXML
+	private TableColumn balanceCol;
 	
 	private static DecimalFormat dec = new DecimalFormat("0.00");
 	
@@ -60,14 +97,45 @@ public class LoanCalcViewController implements Initializable   {
 	@FXML
 	private void btnCalcLoan(ActionEvent event) {
 
-		System.out.println("Amount: " + LoanAmount.getText());
 		double dLoanAmount = Double.parseDouble(LoanAmount.getText());
-		System.out.println("Amount: " + dLoanAmount);	
+		double interestRate = Double.parseDouble(InterestRate.getText())/100.0;
+		int term = Integer.parseInt(NbrOfYears.getText());
+		double additionPmt = Double.parseDouble(addPmt.getText());
+		LocalDate firstDate = fstPmtDate.getValue(); ;
+	    System.out.println("Loan Amount: " + dLoanAmount);	
+		lblTotalPayments.setText("123");
+	    System.out.println("Number of years: " +term);
+	    System.out.println("Interest Rate: "+ interestRate);
+	    System.out.println("First Date: "+ firstDate);
+		Loan myLoan = createLoan(dLoanAmount, interestRate, term, additionPmt, firstDate);
+		setLabels(myLoan);
+		setTable(myLoan, firstDate);
 		
-		lblTotalPayemnts.setText("123");
+	
+	}
+	public void setTable(Loan l, LocalDate ld) {
+		LinkedList<Payment> list = l.getLoanPayments();
+		ObservableList<Payment> oList = FXCollections.observableArrayList(list);
+		principleCol.setCellValueFactory(new PropertyValueFactory<Payment,String>("principle"));
+		balanceCol.setCellValueFactory(new PropertyValueFactory<Payment,String>("balance"));
+		pmtNbrCol.setCellValueFactory(new PropertyValueFactory<Payment,String>("myPmtNbr"));
+		pmtDateCol.setCellValueFactory(new PropertyValueFactory<Payment,String>("pmtDate"));
+		pmtCol.setCellValueFactory(new PropertyValueFactory<Payment, String>("monthPay"));
+		addPmtCol.setCellValueFactory(new PropertyValueFactory<Payment,String>("extraPayment"));
+		interestCol.setCellValueFactory(new PropertyValueFactory<Payment,String>("interest"));
+
+		tblV.setItems(oList);
+			
+	}
+	
+	public void setLabels(Loan l) {
+		lblTotalPayments.setText(dec.format(Math.round(l.calcTotalPayment()*100.0)/100.0));
+		lblTotalInterest.setText(dec.format(Math.round(l.calcInterestPayment()*100.0)/100.0));
+	}
+	public Loan createLoan(double loanAmount, double interestRate, int term, double addPay,LocalDate ld) {
 		
-		LocalDate localDate = PaymentStartDate.getValue();
-	 
-		System.out.println(localDate);
+		return new Loan(loanAmount, interestRate, term, addPay,ld);
+		
 	}
 }
+	
--- StudentLoanCalc/src/main/java/app/helper/Loan.java
+++ StudentLoanCalc/src/main/java/app/helper/Loan.java
@@ -1,9 +1,80 @@
 package app.helper;
 import app.StudentCalc;
 import app.controller.LoanCalcViewController;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.apache.poi.ss.formula.functions.FinanceLib;
 
 public class Loan {
+	double dLoanAmount;
+	double dInterestRate;
+	int iTerm;
+	double dFutureValue;
+	boolean bInterestCalc;
+	double dExtraPayment;
+	double monthlyPayment;
+	LocalDate pmtDate;
 	
-
+	private LinkedList<Payment> loanPayments = new LinkedList<Payment>();
+	
+	public Loan(double loanAmount, double interest,
+			int term, double extraPayment, LocalDate ld) {
+		dLoanAmount = loanAmount;
+		dInterestRate = interest;
+		iTerm = term;
+		dExtraPayment = extraPayment;
+		dFutureValue = 0.0;
+		pmtDate= ld;
+		calcPayment();
+		goLoan();
+	}
+	private void calcPayment() {
+			monthlyPayment = Math.round(-1*FinanceLib.pmt(dInterestRate/12, 12*iTerm, 
+					dLoanAmount, 0, false)*100.0)/100.0;
+}
+	
+	private void goLoan() {
+		
+			do {
+				Payment pay = new Payment(dLoanAmount,dInterestRate,monthlyPayment,dExtraPayment,pmtDate);
+				dLoanAmount = pay.getBalance();
+				loanPayments.add(pay);
+				pmtDate = pmtDate.plusMonths(1);
+			
+		}
+			while(dLoanAmount > (monthlyPayment+dExtraPayment));
+			Payment pay = new Payment(dLoanAmount, dInterestRate, monthlyPayment, dExtraPayment,pmtDate.plusMonths(1));
+			loanPayments.add(pay);
+			pay.setPmtNbr(0);
+	}
+	
+	public LinkedList<Payment> getLoanPayments(){
+		return loanPayments;
+	}
+	
+	public double calcPrinciplePayment() {
+		double princSum = 0.0;
+		for (Payment p : this.getLoanPayments()) 
+		{
+			princSum += p.getPrinciple();
+		}
+		
+		return Math.round(princSum*100.0)/100.0;
+	}
+	
+	public double calcInterestPayment() {
+		double intSum = 0.0;
+		for (Payment p : this.getLoanPayments()) {
+			intSum +=p.getInterest();
+		}
+		
+		return Math.round(intSum * 100.0)/100.0;
+	}
+	
+	public double calcTotalPayment() {
+		return calcPrinciplePayment() + calcInterestPayment();
+	}
 }
